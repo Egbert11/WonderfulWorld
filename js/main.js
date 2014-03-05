1a47ccd98530d1019f5d3e7f1bab31d51540e7ce
unknown <gzs2223@GIH-D-6248.yajuenet.internal>
2014-03-05 20:41:28 +0800

    20:40

--- js/main.js
+++ js/main.js
@@ -5,18 +5,67 @@ $( function(){
     	btnPrev: ".prev",
     	circular: false
 	});
-	//adjustBtnForVcenter();
-	var main = $(".one_line");
-	alert(main.height());
+	adjustBtnForVcenter();
+    initWithData("","{}");
 });
+
 
 function fetchData(){
 
 };
 
-function initWithData(){
-
+function initWithData(url,data){
+	//加载live数据
+    url = "./testdata.php"
+	$.ajax({
+		type:'GET',
+		url:url,
+		data:data,
+		dataType:"json",
+		success:function(data){
+            
+            var container = $("#videolive");
+            var sub_container = $("#videolive .jCarouselLite ul");
+            sub_container.empty();
+            $.each(data,function(index,value){
+              //  alert(index+value);
+                sub_container.append('<li class="cell"id="life_cell'+(index+1)+'"></li>');
+                var cell = sub_container.find("#life_cell"+(index+1));
+                var url = value.imgUrl;
+                var name = value.nickname;
+                var num = value.num;
+                //alert(url+name+num);
+                cell.append('<img class="cell_image" src="'+
+                    url+'"/>');
+                cell.append('<span class="people_name">'+name+'</span>');
+                cell.append('<span class="people_num">'+num +'人</span>');
+                //type = 1;
+                cell.hover(hoverIn,hoverOut);
+        //cell.click();
+            });
+		},
+		error:function(){}
+	});
 };
+
+var hoverIn = function(){
+    var type = $(this).attr('id');
+    type = type.substr(0,type.length-1);
+    switch(type){
+        case 'life_cell':
+             $(this).append('<span class="hover_tiplive">进行直播</span>');
+             break;
+    }
+} 
+
+var hoverOut = function(){
+    var tip = $(this).find(".hover_tiplive");
+                 tip.remove();
+}
+
+var onClickJump = function(){
+    
+}
 
 //compatible for ie 
 function adjustBtnForVcenter(){
