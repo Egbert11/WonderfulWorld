c1090ab744092065b77e895864d3921b1139c886
unknown <gzs2223@GIH-D-6248.yajuenet.internal>
2014-03-11 17:10:07 +0800

    add the popupwindow

--- js/admin.js
+++ js/admin.js
@@ -36,6 +36,7 @@ $(function(){
         var value = $(this).val();
         jumpToSomePage(value);
     });
+    setUpCellFun();
 });
 
 //跳转页面
@@ -45,4 +46,60 @@ function jumpToSomePage(type){
         $(this).removeClass('tab-active');
     });
     $("#"+type).addClass('tab-active');
+}
+
+function openThePopUp(){
+//	alert("abc");
+	initPopUp();
+	var blocklayer = $(".blocklayer");
+	//根据数据修改popup，custom
+	var popUp = $(".popup");
+	blocklayer.css({display:"block"});
+	popUp.css({display:"block"});
+}
+
+function initPopUp(){
+	var blocklayer = $(".blocklayer");
+	//根据数据修改popup，custom
+	var popUp = $(".popup");
+	var closeBtn = popUp.find(".closebtn");
+	closeBtn.click(closeBtnFun);
+	var cancelBtn  = popUp.find(".cancelbtn");
+	cancelBtn.click(closeBtnFun);
+	//custom confirm button behavior when click
+	var confirmBtn = popUp.find(".confirmbtn");
+	confirmBtn.click(closeBtnFun);
+}
+
+
+
+
+
+var  closeBtnFun = function closeThePopUp(){
+	var blocklayer = $(".blocklayer");
+	//根据数据修改popup，custom
+	var popUp = $(".popup");
+	blocklayer.css({display:"none"});
+	popUp.css({display:"none"});
+}
+
+function setUpCellFun(){
+	var container = $(".content");
+	var sub_containers = container.find(".img-wrapper");
+	var l = sub_containers.length;
+	for (var i = 0 ; i < l ; ++i){
+		var sub_container = sub_containers[i];
+		var cells = $("li",sub_container);
+		$.each(cells,function(index,value){
+			var cell = $(this);
+			//点击编辑弹出
+			var editorBtn = cell.find(".edit button");
+			editorBtn.click(function(){
+				var blocklayer = $(".blocklayer");
+				//根据数据修改popup，custom
+				var popUp = $(".popup");
+				openThePopUp();
+			}); 
+		});
+	}
 }
