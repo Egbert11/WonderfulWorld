732828b7580e39cd7679eb0c4b15d00846a36efa
gzwufulin <gzwufulin@corp.netease.com>
2014-03-11 14:48:28 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp -r css /home/ychzhou/server/webservice/wonderfulapi/htdocs
+cp -r image /home/ychzhou/server/webservice/wonderfulapi/htdocs
+cp -r js /home/ychzhou/server/webservice/wonderfulapi/htdocs
+cp index.html /home/ychzhou/server/webservice/wonderfulapi/htdocs
+cp admin.html /home/ychzhou/server/webservice/wonderfulapi/htdocs
